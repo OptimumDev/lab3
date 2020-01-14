1de63b9ea600726bf0fb3cf7b74d9118c653693c
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-01-14 15:51:13 +0500

    refactoring
    ready for code review

--- lab3.ino
+++ lab3.ino
@@ -1,9 +1,8 @@
 #include <LedControl.h>
 
-const int displaysCount = 6;
-const int dataPin = 12;
-const int clkPin = 10;
-const int csPin = 11;
+const int displaysInRow = 6;
+const int rowsCount = 3;
+const int displaySize = 8;
 
 const int echoPin = 2;
 const int trigPin = 3;
@@ -11,54 +10,28 @@ const float voltsPerMeasurement = 5.0 / 1024.0;
 
 const int sensorPin = A0;
 
-bool matrix[24][48];
+const int matrixHeight = rowsCount * displaySize;
+const int matrixWidth = displaysInRow * displaySize;
 
-LedControl lcs[3] = {
-  LedControl(6, 4, 5, displaysCount),
-  LedControl(9, 7, 8, displaysCount),
-  LedControl(12, 10, 11, displaysCount)
+bool matrix[matrixHeight][matrixWidth];
+
+const float speedOfSoundMPerSec = 340.0;
+const float speedOfSoundCmPerUs = speedOfSoundMPerSec / 10000.0;
+
+const int upperDistanceValue = 120;
+
+LedControl lcs[rowsCount] = {
+  LedControl(6, 4, 5, displaysInRow),
+  LedControl(9, 7, 8, displaysInRow),
+  LedControl(12, 10, 11, displaysInRow)
 };
 
 void setup_displays() {
-  for (int j = 0; j < 3; j++) {
-    for (int i = 0; i < displaysCount; i++) {
-      lcs[j].shutdown(i, false);
-      lcs[j].setIntensity(i, 16);
-      lcs[j].clearDisplay(i);
-    }
-  }
-}
-
-void draw(int x, int y, bool is_on) {
-  const int lc_num = y / 8;
-  const int display_num = x / 8;
-  const int display_y = x % 8;
-  const int display_x = 7 - y % 8;
-  lcs[lc_num].setLed(display_num, display_x, display_y, is_on);
-}
-
-void print_matrix() {
-  for (int y = 0; y < 24; y++) {
-    for (int x = 0; x < 48; x++) {
-      Serial.print(matrix[y][x] ? "#" : ".");
-    }
-    Serial.println();
-  }
-}
-
-void draw_matrix() {
-  for (int display_x = 0; display_x < 6; display_x++) {
-    for (int display_y = 0; display_y < 3; display_y++) {
-      for (int y = 0; y < 8; y++) {
-        byte value = 0;
-        for (int x = 0; x < 8; x++) {
-          bool pixel_value = matrix[display_y * 8 + y][display_x * 8 + x];
-          if (!pixel_value)
-            continue;
-          value += (byte) 1 << 7 - x;
-        }
-        lcs[display_y].setRow(display_x, y, value);
-      }
+  for (int i = 0; i < rowsCount; i++) {
+    for (int j = 0; j < displaysInRow; j++) {
+      lcs[i].shutdown(j, false);
+      lcs[i].setIntensity(j, 16);
+      lcs[i].clearDisplay(j);
     }
   }
 }
@@ -74,7 +47,37 @@ void setup()
   setup_displays();
 }
 
-float readPulseUs()
+// drawing
+
+byte count_row_value(int displayX, int displayY, int y) {
+  byte value = 0;
+  for (int x = 0; x < displaySize; x++) {
+    bool pixel_value = matrix[displayY * displaySize + y][displayX * displaySize + x];
+    if (!pixel_value)
+      continue;
+    value += (byte) 1 << displaySize - 1 - x;
+  }
+  return value;
+}
+
+void fill_display(int displayX, int displayY) {
+  for (int y = 0; y < displaySize; y++) {
+    byte value = count_row_value(displayX, displayY, y);
+    lcs[displayY].setRow(displayX, y, value);
+  }
+}
+
+void draw_matrix() {
+  for (int displayX = 0; displayX < displaysInRow; displayX++) {
+    for (int displayY = 0; displayY < rowsCount; displayY++) {
+      fill_display(displayX, displayY);
+    }
+  }
+}
+
+// reading distance
+
+float read_distance_us()
 {
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
@@ -82,55 +85,49 @@ float readPulseUs()
 
   long duration = pulseIn(echoPin, HIGH, 1000 * 1000 * 15);
 
-  return duration;
+  return duration * speedOfSoundCmPerUs / 2.0;
 }
 
-float readDistUs()
-{
-  const float speedOfSoundMPerSec = 340.0;
-  const float speedOfSoundCmPerUs = speedOfSoundMPerSec / 10000.0;
-  return readPulseUs() * speedOfSoundCmPerUs / 2.0;
-}
-
-float readDistIR() {
+float read_distance_ir() {
   float volts = analogRead(sensorPin) * voltsPerMeasurement;
   return pow(14.7737 / volts, 1.2134);
 }
 
-int irGraph[24];
-int usGraph[24];
+// building graph
 
-void add_to_irgraph(float value) {
-  int graph_value = value / 120 * 23;
-  for (int i = 0; i < 23; i++) {
-    irGraph[i] = irGraph[i + 1];
+void shift_matrix_half(int halfNumber) {
+  const int start = matrixWidth / 2 * halfNumber;
+  const int finish = matrixWidth / 2 * (halfNumber + 1) - 1;
+
+  for (int x = start; x < finish; x++) {
+    for (int y = 0; y < matrixHeight; y++) {
+      matrix[y][x] = matrix[y][x + 1];
+    }
   }
-  irGraph[23] = graph_value;
+
+  for (int y = 0; y < matrixHeight; y++) {
+    matrix[y][finish] = false;
+  }
 }
 
-void add_to_usgraph(float value) {
-  int graph_value = value / 120 * 23;
-  for (int i = 0; i < 23; i++) {
-    usGraph[i] = usGraph[i + 1];
-  }
-  usGraph[23] = graph_value;
+int normalize_distance(float distance) {
+  int constrained = constrain(distance, 0, upperDistanceValue);
+  int normalized = constrained / upperDistanceValue * (matrixHeight - 1);
+  return normalized;
+}
+
+void update_graph(int matrixHalfNumber, float distance) {
+  const int matrixColumn = matrixWidth / 2 * (matrixHalfNumber + 1) - 1;
+
+  shift_matrix_half(matrixHalfNumber);
+
+  int graphValue = normalize_distance(distance);
+  matrix[graphValue][matrixColumn] = true;
 }
 
 void loop() {
-  float irDistance = readDistIR();
-  irDistance = constrain(irDistance, 0, 120);
-  add_to_irgraph(irDistance);
-
-  float usDistance = readDistUs();
-  usDistance = constrain(usDistance, 0, 120);
-  add_to_usgraph(usDistance);
-
-  for (int y = 0; y < 24; y++) {
-    for (int x = 0; x < 24; x++) {
-      matrix[23 - y][x] = irGraph[x] == y;
-      matrix[23 - y][24 + x] = usGraph[x] == y;
-    }
-  }
+  update_graph(0, read_distance_ir());
+  update_graph(1, read_distance_us());
 
   draw_matrix();
 }
